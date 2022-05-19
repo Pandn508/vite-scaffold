e2c3f7719987c6b7d592da1f4451f76139f13d44
dingneng.pan <dingneng.pan@yunqutech.com>
2022-05-19 11:17:05 +0800

    feat: 添加vite环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ mode }: ConfigEnv) => {
+  return {
+    envDir: './viteEnv',
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        coms: resolve(__dirname, 'src/components'),
+      },
+      extensions: ['.js', '.json', '.ts', '.vue'],
+    },
+  }
 })
