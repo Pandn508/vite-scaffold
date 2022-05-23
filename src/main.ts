b47a7bb867afe1b87725e2997e15db23230e6a2c
dingneng.pan <dingneng.pan@yunqutech.com>
2022-05-23 16:23:25 +0800

    feat: 添加pinia状态管理

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import { setupStore } from './store'
+
+const app = createApp(App)
+setupStore(app)
+app.use(router)
+app.mount('#app')
